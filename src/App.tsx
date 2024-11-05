6adfe952a4995df08bb3e1478c664cd33450f24c
Anton Belov <anton.belov.al7@gmail.com>
2024-11-05 21:11:55 +0000

    O and X css added

--- src/App.tsx
+++ src/App.tsx
@@ -12,9 +12,25 @@ function App() {
 //   }
 
   return (
-        <main>
-            <div>
+        <main className="h-screen w-screen flex justify-center items-start relative
+        ">
+            <div className="mt-[44vh] -translate-y-[50%] squares-container
+            w-80 h-80
+            sm:w-96 sm:h-96
+            md:w-[30rem] md:h-[30rem]
+            lg:w-[34rem] lg:h-[34rem]
+            ">
+                <div className="square s-t s-l"><div></div></div>
+                <div className="square s-t s-l s-r"><div></div></div>
+                <div className="square s-t s-r"><div></div></div>
 
+                <div className="square s-t s-b s-l"><div></div></div>
+                <div className="square s-t s-b s-l s-r"><div></div></div>
+                <div className="square s-t s-b s-r"><div></div></div>
+
+                <div className="square s-b s-l"><div></div></div>
+                <div className="square s-b s-l s-r"><div></div></div>
+                <div className="square s-b s-r"><div></div></div>
             </div>
         </main>
   );
